e29c4338fbe7edec7dd56f3d085e0f6ef3f79ad0
Abdelrahman Bghdady <bhrabdelrahman8@gmail.com>
2025-10-26 11:42:33 +0300

    fix prefix name in routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Bhry98\KeycloakAuth\Http\Controllers\KeycloakAuthController;
-Route::middleware(['web'])->group(function () {
-    Route::get('/keycloak/login', [KeycloakAuthController::class, 'redirect'])->name('keycloak.login');
-    Route::get('/keycloak/callback', [KeycloakAuthController::class, 'callback'])->name('keycloak.callback');
-});
-//
-//Route::prefix('keycloak')->name('keycloak.')->group(function () {
-//    Route::get('/login', [KeycloakAuthController::class, 'redirect'])->name('login');
-//    Route::get('/callback', [KeycloakAuthController::class, 'callback'])->name('callback');
-//    Route::get('/logout', [KeycloakAuthController::class, 'logout'])->name('logout');
-//});
+
+Route::middleware(['web'])
+    ->prefix('auth/keycloak')
+    ->name('keycloak.')
+    ->group(function () {
+        Route::get('/login', [KeycloakAuthController::class, 'redirect'])->name('login');
+        Route::get('/callback', [KeycloakAuthController::class, 'callback'])->name('callback');
+        Route::get('/logout', [KeycloakAuthController::class, 'logout'])->name('logout');
+    });
